9cf508fc73e70651ce3f95baad7bf5f695d31ef1
ErickGiffoni <giffoni.erick@gmail.com>
2020-09-22 13:03:55 -0300

    erro na linha 63 - programa já faz o exercicio todo
    Co-authored-by: Geise Saunier <geisesaunier3939@gmail.com>

--- Exercícios/multMat.c
+++ Exercícios/multMat.c
@@ -5,6 +5,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -12,84 +13,116 @@
 
 #define item int
 
+pid_t filho1, filho2, idPai;
+item matResultado[2000][2000];
+item mat1[2000][2000];
+item mat2[2000][2000];
+int dimensao;
+
 void imprimeResultado(int signal){
     printf("alguem chamou imprimeResultado\n");
     if(signal == SIGTERM){ //sinal recebido do filho 1
-        pid_t filho1 = getpid(); //aqui esta dando errado : tinha que pegar o ID do filho
-        printf("SIGTERM - quem me chamou foi %d\n", filho1);
+        //pid_t filho1 = getpid(); //aqui esta dando errado : tinha que pegar o ID do filho
+        printf("SIGTERM - sou %d e quem me chamou foi %d\n", getpid(), filho1);
         kill(filho1, SIGPIPE); //filho, imprime a matriz
     }
     else if(signal == SIGALRM){ //sinal recebido do filho 2
-        pid_t filho2 = getpid();
-        printf("SIGALRM - quem me chamou foi %d\n", filho2);
+        //pid_t filho2 = getpid();
+        printf("SIGALRM - sou %d e quem me chamou foi %d\n", getpid(), filho2);
         kill(filho2, SIGSYS); //filho, imprime a matriz
     }
     else if(signal == SIGPIPE){ // filho 1 imprime
-        printf("SIGPIPE recebido de %d\n", getpid());
+        printf("SIGPIPE sou %d\n", getpid());
+        for (int i = 0; i < dimensao; i++){
+            if (i > (dimensao / 2) - 1)
+                break;
+            for (int j = 0; j < dimensao; j++){
+                printf("%d ", matResultado[i][j]);
+            }
+            puts("\n");
+        }
+        //avisar o pai que acabou e morrer
+        kill(idPai, SIGSEGV);
+        exit(0); // ou kill(filho1, SIGKILL)
     }
     else if (signal == SIGSYS){ // filho 2 imprime
-        printf("SIGSYS recebido de %d\n", getpid());
+        printf("SIGSYS sou %d\n", getpid());
+        for (int i = (dimensao/2); i < dimensao; i++){
+            for (int j = 0; j < dimensao; j++){
+                printf("%d ", matResultado[i][j]);
+            }
+            puts("\n");
+        }
+        //avisar o pai que acabou e morrer
+        kill(idPai, SIGBUS);
+        exit(0); // ou kill(filho2, SIGKILL)
+    }
+    else if(signal == SIGSEGV){ //filho 1 imprimiu e morreu
+        //int num;
+        //wait(&num);
+        signal(SIGALRM, imprimeResultado); //quando filho 2 termina a conta
+    }
+    else if(signal == SIGBUS){ //filho 2 imprimiu e morreu
+        //pai espera
+        int num;
+        wait(&num);
     }
 }
 
 int main(){
 
-    //item mat1[2000][2000];
-    //item mat2[2000][2000];
-    //int dimensao = 0;
-    //scanf(" %d", &dimensao);
-    ////ler mat1
-    //for(int i=0; i<dimensao;i++)
-    //    for(int j=0; j<dimensao; j++)
-    //        scanf(" %d", &mat1[i][j]);
-    ////ler mat2
-    //for (int i = 0; i < dimensao; i++)
-    //    for (int j = 0; j < dimensao; j++)
-    //        scanf(" %d", &mat2[i][j]);
-    //
-    //item matResultado[dimensao][dimensao];
+    scanf(" %d", &dimensao);
+    //ler mat1
+    for(int i=0; i<dimensao;i++)
+        for(int j=0; j<dimensao; j++)
+            scanf(" %d", &mat1[i][j]);
+    //ler mat2
+    for (int i = 0; i < dimensao; i++)
+        for (int j = 0; j < dimensao; j++)
+            scanf(" %d", &mat2[i][j]);
 
-    pid_t idPai = getpid();
+    idPai = getpid();
 
     //criar 2 filhos: cada um calcula o produto de um pedaco da matriz
-    pid_t filho1 = fork();
+    filho1 = fork();
 
     if(filho1==0){ // filho 1 calcula da linha 0 ate a dimensao/2 -1
         printf("sou o filho 1 : %d\n e meu pai e : %d\n", getpid(), getppid());
-        //for (int linha = 0; linha < dimensao; linha++){
-        //    if(linha > (dimensao/2)-1 )
-        //        break;
-        //    for (int coluna = 0; coluna < dimensao; coluna++){
-        //        for (int var = 0; var < dimensao; var++){
-        //            matResultado[linha][coluna] += mat1[linha][var] * mat2[var][coluna];
-        //        }
-        //    }
-        //}
+        for (int linha = 0; linha < dimensao; linha++){
+            if(linha > (dimensao/2)-1 )
+                break;
+            for (int coluna = 0; coluna < dimensao; coluna++){
+                for (int var = 0; var < dimensao; var++){
+                    matResultado[linha][coluna] += mat1[linha][var] * mat2[var][coluna];
+                }
+            }
+        }
         //enviar o sinal para papai
         kill(idPai, SIGTERM); //papai, terminei de calcular a matriz
         int num;
-        wait(&num);
+        //wait(&num);
         // receber o sinal do pai para print da matriz
         signal(SIGPIPE, imprimeResultado);
+        wait(&num);
         //signal();
 
     }//end filho 1
 
     else{ //pai
 
-        pid_t filho2 = fork();
+        filho2 = fork();
         if(filho2==0){ //filho 2 calcula de dimensao/2 ate dimensao -1
             printf("sou o filho 2 : %d\ne meu pai e : %d\n", getpid(), getppid());
-            // mat[0][1] * mat2[1][0] += res[0][0]
-            // mat[1][1] * mat2[1][1]
-            // mat[2][1] * mat2[1][2]
-            //for (int linha = (dimensao/2); linha < dimensao; linha++){
-            //    for (int coluna = 0; coluna < dimensao; coluna++){
-            //        for (int var = 0; var < dimensao; var++){
-            //            matResultado[linha][coluna] += mat1[linha][var] * mat2[var][coluna];
-            //        }
-            //    }
-            //}
+            //mat[0][1] * mat2[1][0] += res[0][0]
+            //mat[1][1] * mat2[1][1]
+            //mat[2][1] * mat2[1][2]
+            for (int linha = (dimensao/2); linha < dimensao; linha++){
+                for (int coluna = 0; coluna < dimensao; coluna++){
+                    for (int var = 0; var < dimensao; var++){
+                        matResultado[linha][coluna] += mat1[linha][var] * mat2[var][coluna];
+                    }
+                }
+            }
             //enviar o sinal para papai
             kill(idPai, SIGALRM); //papai, terminei de calcular a matriz
             int num;
@@ -102,9 +135,12 @@ int main(){
         else{//pai fica aqui
             int num;
             printf("sou o pai : %d\n", getpid());
-            signal(SIGTERM, imprimeResultado);
-            signal(SIGALRM, imprimeResultado);
+            signal(SIGTERM, imprimeResultado); //quando filho 1 avisa que terminou a conta
+            signal(SIGSEGV, imprimeResultado); //quando filho 1 imprime e morre
+            //wait(&num);
+            //signal(SIGALRM, imprimeResultado); //quando filho 2 termina a conta
             wait(&num);
+            signal(SIGBUS, imprimeResultado); //quando filho 2 imprime e morre
 
         }//end pai
     }//end geral
